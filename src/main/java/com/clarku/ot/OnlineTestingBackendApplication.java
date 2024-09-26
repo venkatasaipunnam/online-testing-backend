7acd5f854ec3f7c7d0f103159743652ec7499e17
unknown <venkatasai1478@gmail.com>
2024-09-25 22:20:52 -0400

    Initial Project Setup

---
+++ src/main/java/com/clarku/ot/OnlineTestingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clarku.ot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTestingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTestingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarku/ot/OnlineTestingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarku.ot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTestingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
